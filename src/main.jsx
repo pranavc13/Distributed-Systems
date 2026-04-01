ae4fb7a20fbc69f69ea4fb4e1740f37709fdf6f8
pranavc13 <pranavchturvedi18@gmail.com>
2026-04-01 17:40:28 +0530

    errors cleared

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './distributed-systems.jsx'
+import App from './distributed-systems (1)'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
